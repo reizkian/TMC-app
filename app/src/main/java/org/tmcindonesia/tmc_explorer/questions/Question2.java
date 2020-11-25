4009a66217762e8ff926123c0ffb1ac3cbdcb4a6
reizkian <reizkianyesaya@gmail.com>
2020-11-25 16:19:45 +0700

    Added questions page design

---
+++ app/src/main/java/org/tmcindonesia/tmc_explorer/questions/Question2.java
@@ -0,0 +1,15 @@
+package org.tmcindonesia.tmc_explorer.questions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.tmcindonesia.tmc_explorer.R;
+import android.os.Bundle;
+
+public class Question2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question2);
+    }
+}
