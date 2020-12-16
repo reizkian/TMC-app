09231d0a2ac47e31ebe26013d172090c75910469
tcmindonesia <tmc.satuindonesia@gmail.com>
2020-12-16 16:17:34 +0700

    Refractor code prepare for Explorer 2 development

---
+++ app/src/main/java/org/tmcindonesia/app/AcceptJesusAnswer.java
@@ -0,0 +1,66 @@
+package org.tmcindonesia.app;
+
+public class AcceptJesusAnswer {
+    // variable for user answer container
+    private int numberOfCorrectAnswer;
+    private String userAnswerMJWJ1;
+    private String userAnswerMJWJ2;
+    private String userAnswerMJWJ3;
+
+    // constructor
+    public AcceptJesusAnswer(int numberOfCorrectAnswer, String userAnswerMJWJ1, String userAnswerMJWJ2, String userAnswerMJWJ3) {
+        this.numberOfCorrectAnswer = numberOfCorrectAnswer;
+        this.userAnswerMJWJ1 = userAnswerMJWJ1;
+        this.userAnswerMJWJ2 = userAnswerMJWJ2;
+        this.userAnswerMJWJ3 = userAnswerMJWJ3;
+    }
+
+    // to string method: put all variable into a single string
+
+
+    @Override
+    public String toString() {
+        return "numberOfCorrectAnswer=" + numberOfCorrectAnswer +
+                ", userAnswerMJWJ1='" + userAnswerMJWJ1 + '\'' +
+                ", userAnswerMJWJ2='" + userAnswerMJWJ2 + '\'' +
+                ", userAnswerMJWJ3='" + userAnswerMJWJ3 + '\'' +
+                '}';
+    }
+
+    // getter
+    public int getNumberOfCorrectAnswer() {
+        return numberOfCorrectAnswer;
+    }
+
+    public String getUserAnswerMJWJ1() {
+        return userAnswerMJWJ1;
+    }
+
+    public String getUserAnswerMJWJ2() {
+        return userAnswerMJWJ2;
+    }
+
+    public String getUserAnswerMJWJ3() {
+        return userAnswerMJWJ3;
+    }
+
+
+    // setter
+
+    public void setNumberOfCorrectAnswer(int numberOfCorrectAnswer) {
+        this.numberOfCorrectAnswer = numberOfCorrectAnswer;
+    }
+
+    public void setUserAnswerMJWJ1(String userAnswerMJWJ1) {
+        this.userAnswerMJWJ1 = userAnswerMJWJ1;
+    }
+
+    public void setUserAnswerMJWJ2(String userAnswerMJWJ2) {
+        this.userAnswerMJWJ2 = userAnswerMJWJ2;
+    }
+
+    public void setUserAnswerMJWJ3(String userAnswerMJWJ3) {
+        this.userAnswerMJWJ3 = userAnswerMJWJ3;
+    }
+
+}
---
+++ app/src/main/java/org/tmcindonesia/tmc_explorer1/lessons/Lesson8Read.java
@@ -0,0 +1,52 @@
+package org.tmcindonesia.tmc_explorer1.lessons;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.tmcindonesia.tmc_explorer1.R;
+import org.tmcindonesia.tmc_explorer1.questions.LESSON8;
+
+public class Lesson8Read extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pdf_layout);
+
+        // PDF View
+        // pdf-view instance object
+        PDFView pdfView = findViewById(R.id.pdfView);
+        // pdf-view method
+        pdfView.fromAsset("explorer1_published.pdf")
+                .pages(58,59,60,61,62)
+                .enableSwipe(true)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                .enableAnnotationRendering(false)
+                .password(null)
+                .scrollHandle(null)
+                .enableAntialiasing(true)
+                .autoSpacing(false)
+                .fitEachPage(true)
+                .spacing(0)
+                .pageSnap(false)
+                .pageFling(false)
+                .nightMode(false)
+                .load();
+
+        FloatingActionButton fab_goToQuestion;
+        fab_goToQuestion = findViewById(R.id.fab_question);
+        fab_goToQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LESSON8.class));
+            }
+        });
+    }
+}
