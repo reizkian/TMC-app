8846860e123ca73b6e4e6ab787e8571b24c4279e
reizkian <reizkianyesaya@gmail.com>
2021-02-23 18:05:51 +0700

    development-snd: start to layout question page

---
+++ app/src/main/java/org/tmcindonesia/content/tmc_SeeAndDo1/questions/SD1Question1.java
@@ -0,0 +1,16 @@
+package org.tmcindonesia.content.tmc_SeeAndDo1.questions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.tmcindonesia.R;
+
+public class SD1Question1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snd1_question1);
+    }
+}
